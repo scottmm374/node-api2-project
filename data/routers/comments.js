c5eddda6c61189e0466063603ca2a9e2d0e6aef5
Michelle Scott <scottmm374@gmail.com>
2019-12-06 16:14:42 -0500

    Get comment by id works

--- data/routers/comments.js
+++ data/routers/comments.js
@@ -30,6 +30,24 @@ router.get("/", (req, res) => {
 
 // Get Comment by ID
 
-// findCommentById;
+router.get("/:commentsId", (req, res) => {
+  comments
+    .findCommentById(req.params.commentsId)
+    .then(data => {
+      console.log("get by id", data);
+      if (data.length <= 0) {
+        return res.status(404).json({
+          message: "The comment with the specified ID does not exist."
+        });
+      } else {
+        return res.status(200).send(data);
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
+    });
+});
 
 module.exports = router;
